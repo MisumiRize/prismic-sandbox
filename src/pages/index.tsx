aef57475a37084882a6fcec9a36bb35165725657
Rize MISUMI <r@ayase-e.li>
2020-09-28 00:43:23 +0900

    support SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
+import Prismic from 'prismic-javascript'
+import { RichText, RichTextBlock } from 'prismic-reactjs'
 import React from 'react'
+import { GetStaticProps } from 'next'
 
-export default function Index() {
-  return <div>test</div>
+interface Props {
+  docData: {
+    description: RichTextBlock[]
+  }
 }
+
+const Index: React.FC<Props> = ({ docData }: Props) => {
+  return (
+    <div>
+      <RichText render={docData.description} />
+    </div>
+  )
+}
+
+export default Index
+
+const getStaticProps: GetStaticProps<Props> = async () => {
+  const prismicRepo = process.env.PRISMIC_REPO
+  const client = Prismic.client(`https://${prismicRepo}.prismic.io/api/v2`)
+  const doc = await client.getSingle('index', {})
+  return {
+    props: { docData: doc.data }
+  }
+}
+
+export { getStaticProps }
