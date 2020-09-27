c5321e531e0d5ef2b50505d163a5dea886929eec
Rize MISUMI <r@ayase-e.li>
2020-09-27 19:27:09 +0900

    ok

---
+++ src/scripts/generateSchema.ts
@@ -0,0 +1,25 @@
+import fs from 'fs'
+import { getIntrospectionQuery } from 'graphql'
+import fetch from 'isomorphic-fetch'
+import path from 'path'
+import prismic from 'prismic-javascript'
+
+const prismicRepo = process.env.PRISMIC_REPO;
+
+(async function() {
+  const api = await prismic.api(`https://${prismicRepo}.prismic.io/api/v2`)
+  const res = await fetch(
+    `https://${prismicRepo}.prismic.io/graphql?query=${getIntrospectionQuery()}`,
+    {
+      headers: {
+        'Prismic-Ref': api.masterRef.ref,
+      }
+    }
+  )
+  const schema = await res.json()
+  fs.writeFileSync(
+    path.join(__dirname, '../../schema.json'),
+    JSON.stringify(schema, null, 2),
+    'utf8'
+  )
+})()
